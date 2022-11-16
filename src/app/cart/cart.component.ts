627e4f679694bd4417dd9f2415e67ff82c907ff3
tsharks <106757951+tsharks@users.noreply.github.com>
2022-11-16 14:50:07 +0100

    Add purchase functionnality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // TODO: add form processing here
+
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted.', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
-
 }
